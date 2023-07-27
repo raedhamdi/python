4271feb437e3304a87893fa2f74e9a6eda875b0e
raedhamdi <131766401+raedhamdi@users.noreply.github.com>
2023-07-27 19:17:04 +0200

    Add files via upload

---
+++ fundamentals_ for_loop/test.py
@@ -0,0 +1,16 @@
+for i in range (0,151):
+     print (i)
+     for i in range (5,201):
+         print(i*5)
+for i in range (1,101 ):
+    if i % 10==0:
+        print ("coding Dojo")
+    elif i%5 ==0 :
+        print ("codage")
+    else : print(i)
+sum=0
+for i in range(1,500000,2):
+    sum+=i
+print(sum)
+for i in range (2018,0,-4):
+    print (i)
